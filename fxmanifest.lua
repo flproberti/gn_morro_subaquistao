9a421124ea138e1d73462a7a5a735a2e66d788e2
Felipe Roberti Amaral <77501866+flproberti@users.noreply.github.com>
2022-06-08 07:55:33 -0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'bodacious'
+game 'gta5'
+this_is_a_map 'yes'
+
+--[[
+
+Made by: Lil#9587
+Props by: VagnerZN (https://www.fb.gg/vagnerzngamer)
+Discord: https://discord.gg/MYcXRTsPjm
+
+]]
